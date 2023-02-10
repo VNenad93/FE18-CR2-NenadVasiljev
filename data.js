aeb009bfc6a6d64fc7776cca7a94c92121045c7f
vNenad <vasiljevnenad1@gmail.com>
2023-02-10 12:19:24 +0100

    buttons functionality added

--- data.js
+++ data.js
@@ -1,72 +1,72 @@
 let data = `[
     {
     "task_name": "Grocery Shopping",
-    "image": "https://www.example.com/grocery-shopping.jpg",
-    "description": "Buying food and household items",
+    "image": "https://images.unsplash.com/photo-1588964895597-cfccd6e2dbf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    "description": "Buying food and household items, preparing the weekly meny and other accessories",
     "duration": 60,
     "location": "Supermarket",
     "priority": 0
   },
   {
     "task_name": "Lawn Mowing",
-    "image": "https://www.example.com/lawn-mowing.jpg",
-    "description": "Cutting the grass",
+    "image": "https://images.unsplash.com/photo-1501520158826-76df880863a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "description": "Cutting the grass and cleaning around the yard, cut the branches and gather leafs",
     "duration": 30,
     "location": "Backyard",
     "priority": 0
   },
   {
     "task_name": "Gym Workout",
-    "image": "https://www.example.com/gym-workout.jpg",
-    "description": "Exercising at the gym",
+    "image": "https://images.unsplash.com/photo-1580261450046-d0a30080dc9b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=709&q=80",
+    "description": "Exercising at the gym for atleast 45 mins, body weight prior with addition of some weights",
     "duration": 90,
     "location": "Fitness Center",
     "priority": 0
   },
   {
     "task_name": "House Cleaning",
-    "image": "https://www.example.com/house-cleaning.jpg",
-    "description": "Cleaning the house",
+    "image": "https://images.unsplash.com/photo-1581578731548-c64695cc6952?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "description": "Cleaning the house emphasizing on the bathroom and storage and vacum",
     "duration": 120,
     "location": "Home",
     "priority": 0
   },
   {
     "task_name": "Car Wash",
-    "image": "https://www.example.com/car-wash.jpg",
-    "description": "Washing the car",
+    "image": "https://images.unsplash.com/photo-1520340356584-f9917d1eea6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80",
+    "description": "Washing the car inside and outside, check the oil and luggage container",
     "duration": 45,
     "location": "Car Wash",
     "priority": 0
   },
   {
     "task_name": "Doctor's Appointment",
-    "image": "https://www.example.com/doctor-appointment.jpg",
-    "description": "Visiting the doctor",
+    "image": "https://images.unsplash.com/photo-1666214280557-f1b5022eb634?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "description": "Visiting the doctor for the yearly check up",
     "duration": 30,
     "location": "Clinic",
     "priority": 0
   },
   {
     "task_name": "Movie Night",
-    "image": "https://www.example.com/movie-night.jpg",
-    "description": "Watching a movie",
+    "image": "https://images.unsplash.com/photo-1521967906867-14ec9d64bee8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "description": "Watch a movie and relax, order a pizza and drink a beer",
     "duration": 120,
     "location": "Cinema",
     "priority": 0
   },
   {
     "task_name": "Book Club",
-    "image": "https://www.example.com/book-club.jpg",
-    "description": "Reading and discussing books",
+    "image": "https://images.unsplash.com/photo-1495446815901-a7297e633e8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "description": "Get me something new to read, SCI-FI would fit perfect",
     "duration": 90,
     "location": "Library",
     "priority": 0
   },
   {
     "task_name": "Hiking Trip",
-    "image": "https://www.example.com/hiking-trip.jpg",
-    "description": "Going on a hike",
+    "image": "https://images.unsplash.com/photo-1665393717338-dd07628bf667?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    "description": "Going on a 2 hours hike in the woods, have a coffee on top of the hill",
     "duration": 180,
     "location": "Mountain",
     "priority": 0
--- js.js
+++ js.js
@@ -1,14 +1,79 @@
 import data from './data.js';
 
+//parsed data
 const parsedData = JSON.parse(data)
 
-console.log(parsedData)
+for (let task of parsedData) {
 
-for (let i = 0; i < parsedData.length; i++) {
+    document.getElementById('first').innerHTML += `
+    <div>
+        <div class="card p-4">
+        <img src=${task.image} class="card-img-top" alt="${task.description}">
+        <div class="card-body">
+        <h3 class="card-title">${task.task_name}</h3>
+        <p class="card-text" style="border-bottom: 1px solid gray; height: 8rem; padding: 2rem 0">${task.description}</p>
+        <p class="card-text">Location: ${task.location}</p>
+        <p class="card-text">Duration: ${task.duration}</p>
+        <div class="d-flex justify-content-around align-items-center">
+            <h4 class="flex-grow-1 prio">Priority: ${task.priority}</h4>
+            <a href="#" class="btn btn-success" style="width: 3rem; margin-right: 1rem"><h4>+</h4></a>
+            <a href="#" class="btn btn-danger disabled" style="width: 3rem"><h4>-</h4></a>
+        </div>
+        </div>
+        </div>
+    </div>`
+}
 
-    const object = parsedData[i]
+let btnSuccess = document.querySelectorAll('.btn-success')
+let btnDanger = document.querySelectorAll('.btn-danger')
+let prio = document.querySelectorAll('.prio')
 
-    for (let task in object) {
-        console.log(object[task])
+for (let i = 0; i < btnSuccess.length; i++) {
+    btnSuccess[i].addEventListener('click', () => {
+
+        parsedData[i].priority++
+        prio[i].innerHTML = `Priority: ${parsedData[i].priority}`
+
+        color(i)
+        disable(i)
+
+    })
+
+    btnDanger[i].addEventListener('click', () => {
+
+        parsedData[i].priority--
+        prio[i].innerHTML = `Priority: ${parsedData[i].priority}`
+
+        color(i)
+        disable(i)
+    })
+}
+
+
+// Function in charge for determening colors on Priority element
+
+function color(param) {
+    if (parsedData[param].priority < 2) {
+        prio[param].style.color = 'lightgreen'
+    } else if (parsedData[param].priority < 4) {
+        prio[param].style.color = 'goldenrod'
+    } else if (parsedData[param].priority <= 5) {
+        prio[param].style.color = 'tomato'
+    }
+}
+
+// Disabling buttons when the limits are reached
+
+function disable(param) {
+    if (parsedData[param].priority === 5) {
+        btnSuccess[param].classList.add('disabled')
+    } else {
+        btnSuccess[param].classList.remove('disabled')
+    }
+
+    if (parsedData[param].priority > 0) {
+        btnDanger[param].classList.remove('disabled')
+    } else {
+        btnDanger[param].classList.add('disabled')
     }
 }
